0639ceb289971f695dad89861cfd4f78719de8ac
Hans Krohn <hanskrohn70@gmail.com>
2019-05-16 08:53:57 -0500

    Final zoo project with the addition of comments explaining everymethod

--- lib/Animal.rb
+++ lib/Animal.rb
@@ -1,7 +1,9 @@
 class Animal
-    
+    #getter for species and nickname
     attr_reader :species, :nickname
+    # array holding all the Animal Objects
     @@all = []
+    # consturctor for animal objects
     def initialize (species,weight,nickname)
         @species = species
         @weight = weight
@@ -9,26 +11,29 @@ class Animal
         
         @@all << self
     end
+    # getter for current zoo
     def zoo
         @zoo
     end
+    # setter for current zoo
     def zoo=(zoo)
         @zoo=zoo
     end
+    # getter for weight
     def weight
         @weight
     end
+    # setter for weight
     def weight=(weight)
         if weight>=0
             @weight = weight
-        else
-            @weight = 1
         end
     end
+    # getter for every animal object
     def self.all
         @@all
     end
-    
+    # returns all the animals of a spefic species
     def self.find_by_species(species)
         self.all.select do |animal|
             animal.species==species
--- lib/Zoo.rb
+++ lib/Zoo.rb
@@ -1,19 +1,25 @@
 class Zoo
+    # getter for name and location
     attr_reader :name, :location
+    # array that holds every zoo object created
     @@all=[]
+    #  getter for every zoo animal object in array
     def self.all
         @@all
     end
+    # constructor for zoo object
     def initialize (name, location)
         @name=name
         @location=location
         @@all << self
     end
+    # returning every animal object from a specific zoo
     def animals
         Animal.all.select do | animal |
             animal.zoo == self.name
         end
     end
+    # returning the unique species in a specified zoo
     def animal_species
         unique_species =[]
         animals_in_zoo=self.animals
@@ -25,18 +31,21 @@ class Zoo
         end
         unique_species
     end
+    # returns all the animal objects of a specific zoo and species
     def find_by_species(species)
         animals_in_zoo=self.animals
         animals_in_zoo.select do |animals|
             animals.species==species
         end
     end
+    # returns all animals from a specific zoo with a specific nickname
     def animal_nicknames
         animals_in_zoo=self.animals
         animals_in_zoo.map do |animals|
             animals.nickname
         end
     end
+    # returns all the zoo objects that are within the specific location
     def self.find_by_location(location)
         self.all.select do |location_obj|
             location_obj.location == location
