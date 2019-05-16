233ba29b2fce545de39d347d3b0937c4115d5f26
Brendan Brown <pbbrown1112@gmail.com>
2019-05-16 09:27:22 -0500

    Making sure the zoos can find their animals

--- lib/Animal.rb
+++ lib/Animal.rb
@@ -1,3 +1,70 @@
-class Animal
+class Animal    
+
+    @@animals = []
+    
+    def initialize(species, weight, nickname, zoo)
+        @species = species
+        @weight = weight
+        @nickname = nickname
+        @zoo = zoo
+        @@animals << self
+    end
+
+    #Getters
+
+        def species
+            @species
+        end
+
+        def weight
+            @weight 
+        end
+
+        def nickname
+            @nickname
+        end
+
+        def zoo
+            @zoo
+        end
+
+    #End of Getters
+
+    #Setters
+
+        # def species=(species)
+        #     @species=species
+        # end
+
+        def weight=(weight)
+            if weight > 0
+                @weight=weight
+            else
+                puts "Input a weight greater than zero"
+            end
+        end
+
+        # def nickname=(nickname)
+        #     @nickname=nickname
+        # end
+
+        def zoo=(zoo)
+            @zoo=zoo
+        end
+    
+    #End of Setters
+    
+
+    def self.all
+        @@animals
+    end
+
+    def self.find_by_species(species)
+        Animal.all.select do | animal |
+            animal.species == species
+        end
+    end
+
 
 end
+
